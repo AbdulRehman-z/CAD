473678428c2ffa3c5e89f4d5051c531673e547a2
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-14 17:15:59 +0500

    Add generic types

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,9 +1,18 @@
 package p2p
 
-// Peer represents as an interface for a remote node
-type Peer struct{}
+import "net"
+
+// Peer represents as an interface for a remote node.
+type Peer interface{}
 
 // Transport represents as an interface for a transport layer
 type Transport interface {
 	listenAndAccept() error
+}
+
+// Message represents the message that is sent between peers.
+type Message struct {
+	From net.Addr
+	// Payload represents the actual data that is sent between peers.
+	Payload []byte
 }
