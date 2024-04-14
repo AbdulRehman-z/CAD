567e1808fec36f39f39b6b154ad07c1bc26b542b
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-14 17:14:57 +0500

    broken test

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -1,10 +1,17 @@
 package p2p
 
-// func TestTCPTransport(t *testing.T) {
-// 	c := NewTCPTransport(&net.TCPAddr{
-// 		IP:   net.ParseIP("0.0.0.0"),
-// 		Port: 4000,
-// 	})
+import (
+	"net"
+	"testing"
 
-// 	require.Equal(t, "tcp", c.listenAddr.Network(), "Network should be tcp")
-// }
+	"github.com/stretchr/testify/require"
+)
+
+func TestTCPTransport(t *testing.T) {
+	c := NewTCPTransport(&net.TCPAddr{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: 4000,
+	})
+
+	require.Equal(t, "tcp", c.listenAddr.Network(), "Network should be tcp")
+}
