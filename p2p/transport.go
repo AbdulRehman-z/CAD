bc849ee8cc7ead331ee0d8353add431caedc6137
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-16 17:05:10 +0500

    Refactor p2p/transport.go to implement Close() method in Peer interface and add Consume() method in Transport interface

--- p2p/transport.go
+++ p2p/transport.go
@@ -3,15 +3,18 @@ package p2p
 import "net"
 
 // Peer represents as an interface for a remote node.
-type Peer interface{}
+type Peer interface {
+	Close() error
+}
 
 // Transport represents as an interface for a transport layer
 type Transport interface {
 	listenAndAccept() error
+	Consume() <-chan RPC
 }
 
-// Message represents the message that is sent between peers.
-type Message struct {
+// RPC represents the RPC that is sent between peers.
+type RPC struct {
 	From net.Addr
 	// Payload represents the actual data that is sent between peers.
 	Payload []byte
