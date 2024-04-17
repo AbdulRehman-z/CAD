77a3c66fdc47fb8e7ec21a3775939694821f3a4c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-17 20:02:30 +0500

    Add tests for Store and StoreOpts structs in store_test.go

--- store_test.go
+++ store_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"log"
 	"testing"
 
@@ -14,8 +15,51 @@ func TestStore(t *testing.T) {
 	}
 
 	s := NewStore(opts)
-	bytes := bytes.NewReader([]byte("hello there"))
-	if err := s.writeSream("dir", bytes); err != nil {
+	data := []byte("hello there")
+	if err := s.writeSream("dir", bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	r, err := s.Read("dir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, err := io.ReadAll(r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != string(data) {
+		t.Error("data mismatch")
+	}
+
+	if !s.Has("dir") {
+		t.Error("dir should exist")
+	}
+
+	if err := s.Delete("dir"); err != nil {
+		t.Error(err)
+	}
+
+	if s.Has("dir") {
+		t.Error("dir should not exist")
+	}
+
+}
+
+func TestDelete(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CasPathTransformFunc,
+	}
+
+	s := NewStore(opts)
+	data := []byte("hello there")
+	if err := s.writeSream("dir", bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete("dir"); err != nil {
 		t.Error(err)
 	}
 
@@ -25,5 +69,5 @@ func TestCasPathTransformFunc(t *testing.T) {
 	path := "my/path"
 	transformed := CasPathTransformFunc(path)
 	log.Println(transformed)
-	assert.IsType(t, "", transformed)
+	assert.IsType(t, "", transformed.Filename)
 }
