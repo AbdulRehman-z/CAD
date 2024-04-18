57bf163f2a569a78ddc30b7370d482550d17146c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-18 18:29:21 +0500

    Refactor main.go to update TCP port and add FileServer struct

--- main.go
+++ main.go
@@ -2,39 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
+	"log"
 	"net"
+	"time"
 
 	"github.com/1500-bytes/CAD/p2p"
+	"github.com/1500-bytes/CAD/server"
+	"github.com/1500-bytes/CAD/store"
 )
 
 func main() {
-
-	opts := p2p.TCPTransportOpts{
+	transOpts := p2p.TCPTransportOpts{
 		ListenAddr: &net.TCPAddr{
 			IP:   net.ParseIP("0.0.0.0"),
-			Port: 3000,
+			Port: 4000,
 		},
 		Handshake: p2p.NOPEHandshake,
 		Decoder:   &p2p.DefaultDecoder{},
-		OnPeer: func(p2p.Peer) error {
-			fmt.Println("exe")
-			return fmt.Errorf("dropping peer")
-		},
+	}
+	fmt.Printf("Listening on %s\n", transOpts.ListenAddr.String())
+
+	tcpTransport := p2p.NewTCPTransport(transOpts)
+
+	fileServerOpts := server.FileServerOpts{
+		PathTransformFunc: store.CasPathTransformFunc,
+		RootStorage:       "4000_network",
+		Transport:         tcpTransport,
 	}
 
-	t := p2p.NewTCPTransport(opts)
+	s := server.NewFileServer(fileServerOpts)
 
-	if err := t.ListenAndAccept(); err != nil {
-		slog.Error("Fatal Err: ", err)
+	// fmt.Printf("FileServer started with root storage at %s\n", s)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		s.Stop()
+	}()
+
+	log.Println("Starting server")
+	if err := s.Start(); err != nil {
+		log.Fatalf("Error starting server: %s", err)
 	}
 
-	go func(t *p2p.TCPTransport) {
-		for {
-			msg := <-t.Consume()
-			fmt.Printf("Consumed %+v\n", msg)
-		}
-	}(t)
-
-	select {}
 }
