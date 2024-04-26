5739d3fc360d68f1302522a3705e74df75addbf8
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-26 14:06:24 +0500

    Refactor DefaultDecoder.Decode() method in p2p/encoding.go to handle stream detection

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -20,6 +20,17 @@ func (g *GOBDecoder) Decode(r io.Reader, msg *RPC) error {
 type DefaultDecoder struct{}
 
 func (d *DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
+	peekbuf := make([]byte, 1)
+	if _, err := r.Read(peekbuf); err != nil {
+		return nil
+	}
+	stream := peekbuf[0] == IncomingStream
+	if stream {
+		fmt.Println("Stream detected")
+		msg.Stream = true
+		return nil
+	}
+
 	buf := make([]byte, 2000)
 	n, err := r.Read(buf)
 	if err != nil {
@@ -29,3 +40,28 @@ func (d *DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
 	msg.Payload = buf[:n]
 	return nil
 }
+
+// func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
+// 	peekBuf := make([]byte, 1)
+// 	if _, err := r.Read(peekBuf); err != nil {
+// 		return nil
+// 	}
+
+// 	// In case of a stream we are not decoding what is being sent over the network.
+// 	// We are just setting Stream true so we can handle that in our logic.
+// 	stream := peekBuf[0] == IncomingStream
+// 	if stream {
+// 		msg.Stream = true
+// 		return nil
+// 	}
+
+// 	buf := make([]byte, 1028)
+// 	n, err := r.Read(buf)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	msg.Payload = buf[:n]
+
+// 	return nil
+// }
