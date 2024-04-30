cf29e322ee1ead651732bb7f763b705bbda78474
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-30 16:45:45 +0500

    Refactor crypto.go to optimize key generation and hashing

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -3,37 +3,34 @@ package crypto
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/md5"
 	"crypto/rand"
+	"encoding/hex"
 	"io"
 )
 
-func NewEncryptionKey() []byte {
-	key := make([]byte, 16)
-	_, err := rand.Read(key)
-	if err != nil {
-		return nil
-	}
-
-	return key
+func GenerateID() string {
+	buf := make([]byte, 32)
+	io.ReadFull(rand.Reader, buf)
+	return hex.EncodeToString(buf)
 }
 
-func CopyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return 0, err
-	}
+func HashKey(key string) string {
+	hash := md5.Sum([]byte(key))
+	return hex.EncodeToString(hash[:])
+}
 
-	iv := make([]byte, block.BlockSize())
-	if _, err := src.Read(iv); err != nil {
-		return 0, err
-	}
+func NewEncryptionKey() []byte {
+	keyBuf := make([]byte, 32)
+	io.ReadFull(rand.Reader, keyBuf)
+	return keyBuf
+}
 
+func copyStream(stream cipher.Stream, blockSize int, src io.Reader, dst io.Writer) (int, error) {
 	var (
-		buf    = make([]byte, 32*1024)
-		stream = cipher.NewCTR(block, iv)
-		nw     = block.BlockSize()
+		buf = make([]byte, 32*1024)
+		nw  = blockSize
 	)
-
 	for {
 		n, err := src.Read(buf)
 		if n > 0 {
@@ -51,8 +48,24 @@ func CopyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 			return 0, err
 		}
 	}
-
 	return nw, nil
+}
+
+func CopyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return 0, err
+	}
+
+	// Read the IV from the given io.Reader which, in our case should be the
+	// the block.BlockSize() bytes we read.
+	iv := make([]byte, block.BlockSize())
+	if _, err := src.Read(iv); err != nil {
+		return 0, err
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	return copyStream(stream, block.BlockSize(), src, dst)
 }
 
 func CopyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
@@ -61,38 +74,16 @@ func CopyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
 		return 0, err
 	}
 
-	iv := make([]byte, block.BlockSize())
+	iv := make([]byte, block.BlockSize()) // 16 bytes
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return 0, nil
+		return 0, err
 	}
 
+	// prepend the IV to the file.
 	if _, err := dst.Write(iv); err != nil {
 		return 0, err
 	}
 
-	var (
-		buf    = make([]byte, 32*1024)
-		stream = cipher.NewCTR(block, iv)
-		nw     = block.BlockSize()
-	)
-
-	for {
-		n, err := src.Read(buf)
-		if n > 0 {
-			stream.XORKeyStream(buf, buf[:0])
-			nn, err := dst.Write(buf[:n])
-			if err != nil {
-				return 0, err
-			}
-			nw += nn
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return 0, err
-		}
-	}
-
-	return 0, nil
+	stream := cipher.NewCTR(block, iv)
+	return copyStream(stream, block.BlockSize(), src, dst)
 }
