ddeb932f6aa3a313c9c58a6b0c59dc9d9b05edce
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-04-30 16:45:53 +0500

    Refactor Store.StoreData() method to fix data deletion bug

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
@@ -53,24 +54,30 @@ func main() {
 	go s2.Start()
 
 	time.Sleep(1 * time.Second)
-
+	// for i := 0; i < 1; i++ {
 	data := bytes.NewReader([]byte("hello there"))
-	err := s2.StoreData("myprivatedata", data)
+	key := "myprivatedata"
+	err := s2.StoreData(key, data)
 	if err != nil {
 		log.Print(err)
 	}
-	// time.Sleep(5 * time.Millisecond)
 
-	// select {}
-	// r, err := s2.Get("myprivatedata")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	err = s2.Store.Delete(key)
+	if err != nil {
+		log.Print(err)
+	}
+	time.Sleep(2 * time.Second)
 
-	// b, err := io.ReadAll(r)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	// // // select {}
+	r, err := s2.Get(key)
+	if err != nil {
+		log.Println(err)
+	}
 
-	// fmt.Println("Read the following bytes: ", string(b))
+	b, err := io.ReadAll(r)
+	// time.Sleep(2 * time.Second)
+	if err == nil {
+		fmt.Println("Read the following bytes: ", string(b))
+	}
+
 }
